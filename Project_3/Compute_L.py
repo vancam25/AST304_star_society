6e2cbb80214a9680e0fa51dc42995c3fe1e1a2ee
hannah-sulli585 <97410500+hannah-sulli585@users.noreply.github.com>
2022-12-11 15:11:07 -0500

    Updating Compute_L.py
    Added bisectioning.

--- Project_3/Compute_L.py
+++ Project_3/Compute_L.py
@@ -4,27 +4,15 @@
 #### Michigan State University
 """
 
-## INSTRUCTIONS: 
 
-## Write a function that computes Lnuc(R) − 4πR**2σSBTeff**4. Here Lnuc(R) is
-## computed by integrating over the star’s structure, and Teff is found by
-## interpolating from the table of masses and effective temperatures. The
-## root of this equation is the star’s main-sequence radius. 
-## Solve for this radius using a rootfind routine, either scipy.optimize.bisect or its more
-## efficient cousin scipy.optimize.brentq.
+import scipy
+import zams as zms
+import structure as stc
+
+from astro_const import Rsun
 
 
-from zams import Teff 
-from astro_const import fourpi, sigmaSB, G, Msun, Rsun, Lsun, kB, m_u
-from structure import central_thermal, stellar_derivatives, central_values, lengthscales, integrate
-from eos_template import get_rho_and_T, mean_molecular_weight
-from ode import rk4
-from reactions_template import pp_rate
-from scipy import optimize 
-
-
-
-def Compute_L(Mwant, Pc, delta_m, delta_r, eta, xi, mu, XH, pp_factor):
+def Compute_L(Mwant, RWant, def_delta_m, def_delta_r, def_eta, def_xi, comp_array, def_pp_factor, mx):
     
     """
     
@@ -37,21 +25,30 @@ def Compute_L(Mwant, Pc, delta_m, delta_r, eta, xi, mu, XH, pp_factor):
             Interpolated effective temperature of the star in Kelvin [K].
             
     Returns
-        Lnuc(R) - 4πR**2σSBTeff**4 (float, scalar or array)
-            (FINISH THIS DESCRIPTION) 
+        main_sequence_radius (float, scalar) #???
+            Main sequence radius of the star found via bisectioning of Lnuc(R)-4πR**2σSBTeff**4. 
+            In [] ##what are the units dear god help 
     
     """
     
     
-    Teff = Teff(Mwant)
+    teff = zms.Teff(Mwant)
     
-    get_integrate = integrate(Pc, delta_m, delta_r, eta, xi, mu, XH, pp_factor)
+    get_integrate = stc.integrate(Mwant, RWant, def_delta_m, def_delta_r, def_eta, def_xi, comp_array, def_pp_factor, mx)
     Lnuc = get_integrate[3] 
     r_test = get_integrate[1]
     
-    subtraction = Lnuc - fourpi*(R**2)*sigmaSB*(Teff**4)
-    function = (Lnuc/(fourpi*sigmaSB*(Teff**4)))**(1/2)
+    #surf_lum = zms.surface_luminosity(teff,r_test)
+    #subtraction = Lnuc - surf_lum
     
-    main_sequence_radius = scipy.optimize.brentq(function, 0, r_test)
+    def bisect_input(Lnuc, r_test, teff):
+        
+        surf_lum = zms.surface_luminosity(teff,r_test)
+        subtraction = Lnuc - surf_lum
+        
+        return subtraction
+    
+    
+    main_sequence_radius = scipy.optimize.brentq(bisect_input, a = 0, b = def_delta_r, args = ((Lnuc,r_test),teff))
     
     return main_sequence_radius 
