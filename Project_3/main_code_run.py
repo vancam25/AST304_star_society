71d214dc39e6b7304f06745123325f1ee55ca8d5
hannah-sulli585 <97410500+hannah-sulli585@users.noreply.github.com>
2022-12-11 15:17:41 -0500

    Updating main_code_run.py
    Feel free to comment/uncomment my tests at the bottom to see if the radius is being returned correctly.

--- Project_3/main_code_run.py
+++ Project_3/main_code_run.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 
 import structure as stc
 import astro_const as ac
-import additional_funcs as af
+import Compute_L as cl
+import zams as zms
 
 confile = configparser.ConfigParser()
 confile.read('proj_3_config.ini')
@@ -26,18 +27,24 @@ comp_array = np.vstack((Z_array,A_array,XH_array))
 Mwant = 0.3
 RWant = 0.33
 
-# m, r, p, l, rho, T, mx = stc.integrate(Mwant, RWant, def_delta_m, def_delta_r, def_eta, def_xi, comp_array, def_pp_factor, 1000)
+m, r, p, l, rho, T, mx = stc.integrate(Mwant, RWant, def_delta_m, def_delta_r, def_eta, def_xi, comp_array, def_pp_factor, 1000)
 
-# print('m', m[-1] / ac.Msun)
-# print('r', r[-1] / ac.Rsun)
-# print('p', p[-1])
-# print('l', l[-1] / ac.Lsun)
-# print('rho', rho[-1])
-# print('T', T[-1])
-# print('mx', mx)
+#print('m', m[-1] / ac.Msun)
+#print('r', r[-1] / ac.Rsun)
+#print('p', p[-1])
+#print('l', l[-1] / ac.Lsun)
+#print('rho', rho[-1])
+#print('T', T[-1])
+#print('mx', mx)
 
-'''
-Parameter Analysis
-'''
+#test1 = cl.Compute_L(Mwant, RWant, def_delta_m, def_delta_r, def_eta, def_xi, comp_array, def_pp_factor, 1000)
+#print(test1)
 
-af.gen_param_analysis(0.3, 0.333, 1E-20, 'eta')
+teff = zms.Teff(Mwant)
+
+surf_lum = zms.surface_luminosity(teff,r)
+
+print(l-surf_lum)
+
+
+
