05b382da893d338eafeb7bf0763d67143cbaf8d2
StevenVC <sjvancamp@charter.net>
2022-12-11 18:18:14 -0500

    update

--- Project_3/main_code_run.py
+++ Project_3/main_code_run.py
@@ -24,8 +24,8 @@ XH_array = np.array([float(i) for i in confile['Comparray']['Xs'].split(',')])
 
 comp_array = np.vstack((Z_array,A_array,XH_array))
 
-Mwant = 0.4
-Rwant = 0.33
+# Mwant = 0.4
+# Rwant = 0.33
 
 # m, r, p, l, rho, T, mx = stc.integrate(Mwant, Rwant, def_delta_m, def_delta_r, def_eta, def_xi, comp_array, def_pp_factor, 1000)
 
@@ -40,7 +40,8 @@ Rwant = 0.33
 # test1 = cl.Compute_L(Mwant, Rwant, def_delta_m, def_delta_r, def_eta, def_xi, comp_array, def_pp_factor, 10000)
 # print(test1)
 
-test_masses = np.linspace(0.1, 0.3, 3)
+test_masses = np.linspace(0.1, 0.3, 20)
+Rwant = 0.33
 
 main_sequence_radii = np.zeros_like(test_masses)
 main_sequence_Teff = np.zeros_like(test_masses)
@@ -54,3 +55,32 @@ for i in range(len(test_masses)):
 print('R', main_sequence_radii)
 print('Teff', main_sequence_Teff)
 print('Lsurf', main_sequence_Lsurf)
+
+fig, ax1 = plt.subplots()
+
+ax1.scatter(main_sequence_Teff, main_sequence_Lsurf)
+
+ax1.set_xlim(np.max(main_sequence_Teff)*1.2, np.min(main_sequence_Teff)*0.8)
+
+ax1.set_xscale('log')
+ax1.set_yscale('log')
+
+ax1.set_xlabel('Teff')
+ax1.set_ylabel('Lsurf')
+
+ax1.grid()
+
+# ax2.scatter(main_sequence_Teff, main_sequence_Lsurf)
+
+# ax2.set_xlim(np.max(main_sequence_Teff)*1.2, np.min(main_sequence_Teff)*0.8)
+
+# ax2.set_xscale('log')
+# ax2.set_yscale('log')
+
+# ax2.set_xlabel('Teff')
+# ax2.set_ylabel('Lsurf')
+
+# ax2.grid()
+
+# fig.savefig('FinalPlots/Mass_Radius_Plot.svg')
+# plt.clf()
